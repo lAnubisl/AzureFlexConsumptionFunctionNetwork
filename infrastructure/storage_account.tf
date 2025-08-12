8defb413295a3a044e867b8cae45febbae601974
Alexander Panfilenok <alexander.panfilenok@gmail.com>
2025-08-12 12:02:39 +0000

    Update storage blob resource to use module path for source and add dependency on role assignment

--- infrastructure/storage_account.tf
+++ infrastructure/storage_account.tf
@@ -57,9 +57,10 @@ resource "azurerm_role_assignment" "ra_curr_sc_metadata" {
 }
 
 resource "azurerm_storage_blob" "test_blob" {
+  depends_on             = [azurerm_role_assignment.ra_curr_sc_metadata]
   name                   = "test.txt"
   storage_account_name   = azurerm_storage_account.st_metadata.name
   storage_container_name = azurerm_storage_container.sc_metadata.name
   type                   = "Block"
-  source                 = "test.txt"
+  source                 = "${path.module}/test.txt"
 }
