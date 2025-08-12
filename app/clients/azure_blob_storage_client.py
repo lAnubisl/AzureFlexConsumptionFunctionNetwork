46271c97f4c8fb4cd2bf6197b75d8879d6205a02
Alexander Panfilenok <alexander.panfilenok@gmail.com>
2025-08-12 15:27:47 +0000

    Refactor AzureBlobStorageClient to initialize BlobServiceClient within the read method for improved resource management

--- app/clients/azure_blob_storage_client.py
+++ app/clients/azure_blob_storage_client.py
@@ -2,7 +2,7 @@ import logging
 from typing import Optional
 from opentelemetry.trace import Tracer, SpanKind
 from azure.identity.aio import DefaultAzureCredential
-from azure.storage.blob.aio import BlobServiceClient, BlobClient, ContainerClient, StorageStreamDownloader
+from azure.storage.blob.aio import BlobServiceClient, BlobClient, StorageStreamDownloader
 from helpers.config_helper import ConfigHelper
 
 class AzureBlobStorageClient:
@@ -11,11 +11,6 @@ class AzureBlobStorageClient:
         self.__config: ConfigHelper = config
         self.__logger: logging.Logger = logger
         self.__tracer: Tracer = tracer
-        account_url = "https://<storageaccountname>.blob.core.windows.net"
-        default_credential = DefaultAzureCredential()
-
-        # Create the BlobServiceClient object
-        self.__blob_service_client = BlobServiceClient(account_url, credential=default_credential)
 
     async def read(self) -> Optional[str]:
         """
@@ -23,11 +18,16 @@ class AzureBlobStorageClient:
         """
         try:
             with self.__tracer.start_as_current_span("read_blob", kind=SpanKind.CLIENT):
-                container_client: ContainerClient = self.__blob_service_client.get_container_client(self.__config.get_storage_container_name())
-                blob_client: BlobClient = container_client.get_blob_client("text.txt")
-                downloader: StorageStreamDownloader[str] = await blob_client.download_blob(max_concurrency=1, encoding='UTF-8')
-                content: str = await downloader.readall()
-                return content
+                async with DefaultAzureCredential() as credential:
+                    account_name = self.__config.get_storage_account_name()
+                    account_url = f"https://{account_name}.blob.core.windows.net"
+                    with BlobServiceClient(account_url, credential=credential) as blob_service_client:
+                        container_name = self.__config.get_storage_container_name()
+                        with blob_service_client.get_container_client(container_name) as container_client:
+                            blob_client: BlobClient = container_client.get_blob_client("text.txt")
+                            downloader: StorageStreamDownloader[str] = await blob_client.download_blob(max_concurrency=1, encoding='UTF-8')
+                            content: str = await downloader.readall()
+                            return content
         except Exception as e:
             self.__logger.error(f"Error reading blob: {e}")
             return None
