860db27895262bafade41d64e6101c4048e8ced4
Alexander Panfilenok <alexander.panfilenok@gmail.com>
2025-08-12 11:37:37 +0000

    Fix typos in logging.tf for resource references and correct tags assignment

--- infrastructure/logging.tf
+++ infrastructure/logging.tf
@@ -4,14 +4,13 @@ resource "azurerm_log_analytics_workspace" "log" {
   resource_group_name = azurerm_resource_group.rg.name
   sku                 = "PerGB2018"
   retention_in_days   = 30
-  tags                = module.naming.tags
 }
 
 resource "azurerm_application_insights" "appi" {
   name                = "appi-${random_string.random.result}"
-  location            = aazurerm_resource_group.rg.location
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  workspace_id        = azurerm_log_analytics_workspace.logs.id
+  workspace_id        = azurerm_log_analytics_workspace.log.id
   application_type    = "other"
   retention_in_days   = 30
 }
