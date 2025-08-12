a015da80bb74f77aacbb37fdaa505a136b45a28d
Alexander Panfilenok <alexander.panfilenok@gmail.com>
2025-08-12 11:47:07 +0000

    Add role assignment for storage container access in Terraform configuration

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -10,3 +10,6 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-${random_string.random.result}"
   location = var.resource_group_location
 }
+
+data "azurerm_client_config" "current" {
+}
--- infrastructure/storage_account.tf
+++ infrastructure/storage_account.tf
@@ -50,6 +50,12 @@ resource "azurerm_private_endpoint" "private_endpoint_st_metadata" {
   }
 }
 
+resource "azurerm_role_assignment" "ra_curr_sc_metadata" {
+  scope                = azurerm_storage_container.sc_metadata.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = data.azurerm_client_config.current.object_id
+}
+
 resource "azurerm_storage_blob" "test_blob" {
   name                   = "test.txt"
   storage_account_name   = azurerm_storage_account.st_metadata.name
