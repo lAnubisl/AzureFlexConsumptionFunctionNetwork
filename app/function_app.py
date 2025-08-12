c6745f8e5b60827fbf78ce8039d33b93e6042200
Alexander Panfilenok <alexander.panfilenok@gmail.com>
2025-08-12 20:57:56 +0000

    Refactor timer_trigger function to improve context management and logging

--- app/function_app.py
+++ app/function_app.py
@@ -1,28 +1,32 @@
 import logging
 from opentelemetry import trace
+from opentelemetry.trace import SpanKind
 import azure.functions as func
 from azure.monitor.opentelemetry import configure_azure_monitor # type: ignore
-from ambient_context_manager import set_context, unset_context
 from utils import transform_context
 from clients.azure_blob_storage_client import AzureBlobStorageClient
 from helpers.config_helper import ConfigHelper
 
 configure_azure_monitor()
 
+# disable logging from library azure
+logging.getLogger("azure").setLevel(logging.ERROR)
+
 app = func.FunctionApp()
+logger = logging.getLogger("MyLogger")
+tracer = trace.get_tracer("MyLogger")
+client = AzureBlobStorageClient(
+    config=ConfigHelper(),
+    logger=logger,
+    tracer=tracer
+)
 
 @app.timer_trigger(schedule="0 * * * * *", arg_name="myTimer") 
 async def timer_trigger(myTimer: func.TimerRequest, context: func.Context) -> None:
-    set_context(transform_context(context))
-    try:
-        log = logging.getLogger("MyLogger")
-        client = AzureBlobStorageClient(
-            config=ConfigHelper(),
-            logger=log,
-            tracer=trace.get_tracer("MyLogger")
-        )
-
-        content: str = await client.read()
-        log.info(f"Blob content: {content}")
-    finally:
-        unset_context()
+    ctx = transform_context(context)
+    with tracer.start_as_current_span("timer_trigger", context=ctx, kind=SpanKind.INTERNAL):
+        try:
+            content: str = await client.read()
+            logger.info(f"Blob content: {content}")
+        except Exception as e:
+            logger.error(f"Error in timer trigger: {e}")
